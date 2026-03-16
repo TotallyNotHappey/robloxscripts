7c9f842ca1b0d87f574b69fb38858885b3624c2a
Happey <jingwenchen365@gmail.com>
2026-03-17 01:17:22 +0800

    Add files via upload

---
+++ uhhhhhhreanim/biast.lua
@@ -0,0 +1,45 @@
+-- UhhhhhhReanim/Modules/biast.lua
+
+local modules = {}
+
+table.insert(modules, function()
+    local m = {}
+
+    m.ModuleType  = "DANCE"
+    m.Name        = "Biast lag train"
+    m.Description = "yall WHO is this 🤷"
+    m.Assets = {"LagTrainDiff.anim", "lagtraindiff.mp3"}
+
+    m.Config = function(parent)
+        Util_CreateText(parent, "No settings.", 14, Enum.TextXAlignment.Center)
+    end
+
+    m.SaveConfig = function() return {} end
+    m.LoadConfig  = function(save) end
+
+    local animator = nil
+    local start    = 0
+
+    m.Init = function(figure)
+        SetOverrideDanceMusic(AssetGetContentId("lagtraindiff.mp3"), "lagtraindiff", 0.8, NumberRange.new(0, 45.5))
+
+        start           = os.clock()
+        animator        = AnimLib.Animator.new()
+        animator.rig    = figure
+        animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("LagTrainDiff.anim"))
+        animator.looped = true
+        animator.speed  = 1
+    end
+
+    m.Update = function(dt, figure)
+        animator:Step(os.clock() - start)
+    end
+
+    m.Destroy = function(figure)
+        animator = nil
+    end
+
+    return m
+end)
+
+return modules
